050157be4768dce9d42505d80fde955c0e6e3e88
ulaecio <ulaecio@live.com>
2022-03-22 00:44:21 -0300

    Project human-resource-worker created

---
+++ human-resource-worker/src/main/java/com/ulasoftware/humanresourceworker/HumanResourceWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.humanresourceworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceWorkerApplication.class, args);
+	}
+
+}
---
+++ human-resource-worker/src/test/java/com/ulasoftware/humanresourceworker/HumanResourceWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.humanresourceworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
