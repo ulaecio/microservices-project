6c96836b5f79c81a1399cdc22a94de25fe1761f9
ulaecio <ulaecio@live.com>
2022-05-19 22:29:39 -0300

    Ribbon load balancing

--- human-resource-payroll/src/main/java/com/ulasoftware/humanresourcepayroll/HumanResourcePayrollApplication.java
+++ human-resource-payroll/src/main/java/com/ulasoftware/humanresourcepayroll/HumanResourcePayrollApplication.java
@@ -2,8 +2,12 @@ package com.ulasoftware.humanresourcepayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.stereotype.Component;
 
+@Component
+@RibbonClient(name = "human-resource-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HumanResourcePayrollApplication {
--- human-resource-worker/src/main/java/com/ulasoftware/humanresourceworker/resources/WorkerResource.java
+++ human-resource-worker/src/main/java/com/ulasoftware/humanresourceworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.ulasoftware.humanresourceworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,12 @@ import com.ulasoftware.humanresourceworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
+	//Impressão de coisas no log
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private WorkerRepository repository;
 	 
@@ -26,6 +35,10 @@ public class WorkerResource {
 	}
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+		//imprime a porta localhost usada
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
