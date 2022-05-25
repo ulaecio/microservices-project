2c301fbe3a0556e30cb8605a5f9325bb97753922
ulaecio <ulaecio@live.com>
2022-05-25 00:05:18 -0300

    Configuring eureka server

--- human-resource-eureka-server/src/main/java/com/ulasoftware/humanresourceeurekaserver/HumanResourceEurekaServerApplication.java
+++ human-resource-eureka-server/src/main/java/com/ulasoftware/humanresourceeurekaserver/HumanResourceEurekaServerApplication.java
@@ -2,7 +2,11 @@ package com.ulasoftware.humanresourceeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import jdk.jfr.Enabled;
+
+@EnableEurekaServer
 @SpringBootApplication
 public class HumanResourceEurekaServerApplication {
 
