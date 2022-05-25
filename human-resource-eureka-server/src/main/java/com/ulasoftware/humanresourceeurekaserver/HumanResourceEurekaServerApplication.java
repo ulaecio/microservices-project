b8eee4015978a270cd56e2936e589d38bedb327b
ulaecio <ulaecio@live.com>
2022-05-24 22:03:08 -0300

    HR eureka server project created

---
+++ human-resource-eureka-server/src/main/java/com/ulasoftware/humanresourceeurekaserver/HumanResourceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.humanresourceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceEurekaServerApplication.class, args);
+	}
+
+}
---
+++ human-resource-eureka-server/src/test/java/com/ulasoftware/humanresourceeurekaserver/HumanResourceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulasoftware.humanresourceeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
